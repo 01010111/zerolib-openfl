d7eecfc427ca3c04f055e7dfcea6d25180f3c740
01010111 <will.blanton.804@gmail.com>
2021-02-11 14:26:42 -0500

    fold UpdateManager into Game

--- zero/openfl/utilities/Game.hx
+++ zero/openfl/utilities/Game.hx
@@ -1,5 +1,9 @@
 package zero.openfl.utilities;
 
+import zero.utilities.Tween;
+import zero.utilities.Tween.TweenProperty;
+import zero.utilities.Timer;
+import openfl.events.Event;
 import zero.utilities.Vec2;
 import openfl.display.Sprite;
 import zero.openfl.utilities.Scene;
@@ -25,21 +29,23 @@ class Game {
 	#end
 
 	public static var root:Sprite;
-
 	public static var mouse:Vec2 = [0, 0];
+
+	var last = Date.now().getTime();
 
 	public function new(root:Sprite, scene:Class<Scene>) {
 		i = this;
 		Game.root = root;
+
+		root.addEventListener(Event.ENTER_FRAME, tick);
+		root.addEventListener(Event.EXIT_FRAME, (e) -> 'post_update'.dispatch());
+		root.addEventListener(Event.RESIZE, (e) -> 'resize'.dispatch({ width: width, height: height }));
 
 		#if echo
 		world = new echo.World({
 			width: Game.width,
 			height: Game.height
 		});
-
-		var update = (?dt) -> world.step(dt);
-		update.listen('update');
 		#end
 
 		change_scene(Type.createInstance(scene, []));
@@ -54,6 +60,18 @@ class Game {
 		var fps = new FPS(32, Game.height - 80, 0xFFFFFF);
 		root.add(fps);
 		((?_) -> fps.y = Game.height - 80).listen('resize');
+		#end
+	}
+
+	function tick(e:Event) {
+		var time = Date.now().getTime();
+		var dt = (time - last) / 1000;
+		last = time
+		Timer.update(dt);
+		Tween.update(dt);
+		'update'.dispatch(dt);
+		#if echo
+		if (world != null) world.step(dt);
 		#end
 	}
 	
