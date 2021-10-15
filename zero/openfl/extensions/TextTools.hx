a59c326dab7bead2eb390cde581c51804bfbb905
01010111 <will.blanton.804@gmail.com>
2021-10-15 10:10:49 -0400

    use defaultTextFormat

--- zero/openfl/extensions/TextTools.hx
+++ zero/openfl/extensions/TextTools.hx
@@ -21,7 +21,7 @@ class TextTools {
 	// Static Extensions
 
 	public static function format(text:TextField, options:TextFormatOptions, cache:String = '') {
-		text.setTextFormat(get_format_from_options(options));
+		text.defaultTextFormat = get_format_from_options(options);
 		if (cache.length > 0) store_format(cache, options);
 		text.autoSize = switch options.align {
 			case null | JUSTIFY | LEFT | START : LEFT;
